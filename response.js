98d70ffb32ab494176d18e1ccdb35d6a465816f0
Sharlotte <60801210+Sharlottes@users.noreply.github.com>
2022-05-05 01:28:16 +0900

    Update response.js

--- response.js
+++ response.js
@@ -1,7 +1,8 @@
+"use strict";
+
 importPackage(org.jsoup);
 
 const bot = BotManager.getCurrentBot();
-const manager = new CommandManager();
 const Kakao = new (require('kaling'))();
 const secret = Database.readObject("./secret.json");
 try{ //전체 명령어의 카링 로그인에 대한 의존성 방지
@@ -19,6 +20,7 @@ const reg = /[\"\[\{\}\[\]\/?\.\,\;\:\|\)\*\~\`\!\^\-\_\+\<\>\@\#\$\%\&\\\=\(\'\
 //Eval 권한
 const perm = ['-472599725'];
 
+const { CLOUDINARY_CLIENT_ID, NAVER_CLIENT_ID, NAVER_CLIENT_SECRET, NAVER_KEY, GOOGLE_KEY, MATTERS_KEY, SEARCH_ID, GITHUB_KEY } = secret;
 
 //Utils
 String.prototype.trip = function(step) {
@@ -43,6 +45,7 @@ function init() {
     new BaseCommand(["상태확인", "status"], status),
     new BaseCommand(["대화기록", "cl"], chatlog, new Option("필터").setOptional()),
     new BaseCommand(["코로나", "corona"], corona),
+    new BaseCommand(["검색", "search"], search, [new Option("유형"), new Option("검색어"), new Option("옵션")])
   ];
 }
 
@@ -59,7 +62,7 @@ function init() {
  * @param {string} room 수신할 방 이름
  * @param {ListParameter} body 수신 인자
 */
-function send(room, body) {
+const send = function (room, body) {
   const args = {};
   if(body.url) args.URL = body.url;
   if(body.title) args.TITLE = body.title;
@@ -75,10 +78,35 @@ function send(room, body) {
   }, 'custom');
 }
 
+/**
+ * 
+ * @param {Connection.Request} data - 요청 인자 
+ * @returns {Connection.Response}
+ */
+const request = function (data) {
+  let con = Jsoup.connect(data.url).ignoreContentType(true).ignoreHttpErrors(true).method(Connection.Method.GET);
+  if(data.requestBody) con = con.requestBody(data.requestBody);
+  if(data.data) con = con.data(data.data);
+  if(data.header) con = con.headers(data.header);
+  if(data.method) con = con.method(Connection.Method.valueOf(data.method));
+  if(data.parser) con = con.parser(data.parser);
+  if(data.proxy) con = con.proxy(data.proxy);
+  if(data.timeout) con = con.timeout(data.timeout);
+  if(data.cookies) con = con.cookies(data.cookies);
+  if(data.userAgent) con = con.userAgent(data.userAgent);
+
+  return con.execute();
+}
+
+const requestObj = function (data) {
+  return JSON.parse(new java.lang.String(request(data).bodyAsBytes(), "UTF-8"));
+}
+
 //명령어 관리 클래스
-function CommandManager() {
+const CommandManager = function () {
   this.commands = [];
 }
+const manager = new CommandManager();
 
 /**
 명령어 옵션 클래스
@@ -89,17 +117,16 @@ const Option = function(name, type) {
   this.name = name;
   this.type = type||"string";
   this.optional = false;
-  this.typeValid = arg => {
-    if(type === "string") return true;
-    if(type === "int") return !arg.replace(/\d/g, "");
-    return !arg.replace(/\d|[\d+\.\d+]/g, "");
-  }
-  this.setOptional = function() {
-    this.optional = true;
-    return this;
-  }
-
   if(this.type != 'string' && this.type != 'int' && this.type != 'float') throw new Error("옵션 생성자의 type 인자는 string, int, float 중 하나여야만 합니다: "+this.type);
+}
+Option.prototype.typeValid = function(arg) {
+  if(type === "string") return true;
+  if(type === "int") return !arg.replace(/\d/g, "");
+  return !arg.replace(/\d|[\d+\.\d+]/g, "");
+}
+Option.prototype.setOptional = function() {
+  this.optional = true;
+  return this;
 }
 
 
@@ -154,13 +181,15 @@ BaseCommand.prototype.isValid = function(msg) {
     
     if(this.prefix.some(p=>this.names.includes(spliten[0].slice(p.length)))) {
       if(!this.options.length) return true;
+      
       for(let i = 0; i < this.options.length; i++) {
         const opt = this.options[i];
         if(opt.optional || (args[i] && typeof args[i] === opt.type)) return true;
       }
-      let str = this.prefix.join(",")+this.names.join("|");
-      if(this.options.length) str += " "+this.options.map(opt=>(opt.optional?"[":"(")+opt.name+":"+opt.type+(opt.optional?"]":")")).join(this.saperator.toString().trip());
-      msg.reply(str);
+      
+      //유효하지 않은 서식은 도움말 답변
+      if(this.options.length) msg.reply(this.prefix.join(",")+this.names.join("|")+" "+this.options.map(opt=>(opt.optional?"[":"(")+opt.name+":"+opt.type+(opt.optional?"]":")")).join(this.saperator.toString().trip()));
+      else msg.reply(this.prefix.join(",")+this.names.join("|"));
     }
   }
 }
@@ -177,7 +206,7 @@ BaseCommand.prototype.addPrefix = function(prefix) {
 }
 
 
-function help(msg) {
+const help = function (msg) {
   msg.reply(manager.commands.map(cmd=>{
     let str = "• "+cmd.prefix.join(",")+cmd.names.join("|");
     if(cmd.options.length) str += " "+cmd.options.map(opt=>(opt.optional?"[":"(")+opt.name+":"+opt.type+(opt.optional?"]":")")).join(cmd.saperator.toString().trip());
@@ -185,12 +214,13 @@ function help(msg) {
   }).join("\n"));
 }
 
-function roomInfo(msg) {
-  const data = JSON.parse(Jsoup.connect("https://api.develope.kr/search/room?room="+msg.room)
-    .ignoreContentType(true)
-    .ignoreHttpErrors(true)
-    .get().text());
-  
+const roomInfo = function (msg) {
+  const data = requestObj({
+    url: "https://api.develope.kr/search/room",
+    data: {
+      room: msg.room
+    }
+  });
   if(data.success&&data.result) {
     let obj = data.result;
     send(msg.room, {
@@ -207,7 +237,7 @@ function roomInfo(msg) {
   }
 }
 
-function disaster(msg, args) {
+const disaster = function (msg, args) {
   const end = new Date();
   const start = new Date(end - (7 * 24 * 60 * 60 * 1000));
   const req = JSON.stringify({
@@ -217,12 +247,14 @@ function disaster(msg, args) {
         searchEndDe: end.getFullYear()+"-"+(end.getMonth()+1)+"-"+end.getDate()
     }
   });
-  const data = JSON.parse(Jsoup.connect('https://www.safekorea.go.kr/idsiSFK/sfk/cs/sua/web/DisasterSmsList.do') 
-    .header('Content-Type', 'application/json;charset=utf-8')
-    .requestBody(req)
-    .ignoreContentType(true)
-    .ignoreHttpErrors(true)
-    .post().text());
+  const data = requestObj({
+    url: "https://www.safekorea.go.kr/idsiSFK/sfk/cs/sua/web/DisasterSmsList.do",
+    requestBody: req,
+    method: 'POST',
+    header: {
+      'Content-Type': 'application/json;charset=utf-8'
+    }
+  });
   msg.reply(data.disasterSmsList.filter(e=>!args[0]||e.RCV_AREA_NM.includes(args[0])).map((sms,i,arr)=>
     sms.DSSTR_SE_NM+" | "+sms.CREAT_DT
     +"\n"+ sms.MSG_CN
@@ -230,7 +262,7 @@ function disaster(msg, args) {
   ).join("\n\n\n"));
 }
 
-function teach(msg, args) {
+const teach = function (msg, args) {
   const room = "study-"+msg.room.slice(0, 6).trim().toLowerCase().replace(reg, "").replace(/\s/g,"")+msg.room.length;
   const list = Database.exists(room+".json") ? JSON.parse(Database.readString(room+".json")) : {};
   const [ key, value ] = args;
@@ -257,7 +289,7 @@ function teach(msg, args) {
   Database.writeString(room+".json", JSON.stringify(list));
 }
 
-function teachList(msg, args) {
+const teachList = function (msg, args) {
   const room = "study-"+msg.room.slice(0, 6).trim().toLowerCase().replace(reg, "").replace(/\s/g,"")+msg.room.length;
   const list = Database.exists(room+".json") ? JSON.parse(Database.readString(room+".json")) : {};
   const [ sender ] = args;
@@ -268,7 +300,7 @@ function teachList(msg, args) {
   : `${getData(keys)}`))
 }
 
-function status(msg) {
+const status = function (msg) {
   let str = "봇 상태";
   str += `\n• 기기 베터리: ${Device.getBatteryLevel()}%`;
   if(Device.isCharging()) str+=" (충전 중)";
@@ -276,7 +308,7 @@ function status(msg) {
   msg.reply(str);
 }
 
-function chatlog(msg, args) {
+const chatlog = function (msg, args) {
   const room = msg.room.slice(0, 6).trim().toLowerCase().replace(reg, "").replace(/\s/g,"")+msg.room.length;
   let filterstr = [];
   let reverse = true, viewDate = false, viewPre = false, sendImg = false;
@@ -354,7 +386,7 @@ function chatlog(msg, args) {
   const image = chats.reverse().find(c=>c.image);
   if(sendImg) {
     if(!image) return msg.reply("이미지가 없습니다!");
-    const url = `https://res.cloudinary.com/${secret.cloudinary}/image/upload/`+image.id;
+    const url = `https://res.CLOUDINARY_CLIENT_ID.com/${CLOUDINARY_CLIENT_ID}/image/upload/`+image.id;
     
     send(msg.room, {
       url: url,
@@ -364,7 +396,7 @@ function chatlog(msg, args) {
   }
 }
 
-function executeEval(msg) {  
+const executeEval = function (msg) {  
   const userhash = java.lang.String(msg.author.avatar.getBase64()).hashCode();
   if(perm.includes(""+userhash)){
     try {
@@ -378,9 +410,11 @@ function executeEval(msg) {
   else msg.reply("권한 미달: "+userhash);
 }
 
-function corona(msg) {
+const corona = function (msg) {
   const vaccinate = [];
-  const vac = Jsoup.connect("http://ncov.mohw.go.kr/").get().body();
+  const vac = request({
+    url: "http://ncov.mohw.go.kr/"
+  });
   const child = vac.select("div.child_list").select("percent").toArray();
   let i = 0;
   vac.select("div.vaccine_list > div.inner.cl_b_aftr > div.box").forEach(ele => {
@@ -395,7 +429,7 @@ function corona(msg) {
   msg.reply(`백신 현황\n==========\n${vaccinate.join("\n")}\n\n확진/사망 현황\n==========\n확진: +${dayConfirm.ownText()}명 (누적 ${totalConfirm.ownText()}명)\n사망: +${dayDead.ownText()}명 (누적 ${totalDead.ownText()}명)`);
 }
 
-function onMessage(msg) {
+const onMessage = function (msg) {
   const cont = msg.content;
   const room = msg.room;
   if(cont.includes("라그랑주")) {
@@ -430,11 +464,11 @@ function onMessage(msg) {
   }
   else if(cont.startsWith("몰?루")) {
     const molu = [
-      ["모?루", `https://res.cloudinary.com/${secret.cloudinary}/image/upload/v1637820695/1637601279492_cugxuz.jpg`],
-      ["쇼핑몰?루", `https://res.cloudinary.com/${secret.cloudinary}/image/upload/v1637874000/1637873933719_fxvncd.jpg`],
-      ["뿅!", `https://res.cloudinary.com/${secret.cloudinary}/image/upload/v1637874683/images_1_qzx3hm.jpg`],
-      ["몰?루없음", `https://res.cloudinary.com/${secret.cloudinary}/image/upload/v1637874661/images_bntlm7.jpg`],
-      ["모모?코", `https://res.cloudinary.com/${secret.cloudinary}/image/upload/v1637874697/i14615767089_r4plwp.png`]
+      ["모?루", `https://res.CLOUDINARY_CLIENT_ID.com/${CLOUDINARY_CLIENT_ID}/image/upload/v1637820695/1637601279492_cugxuz.jpg`],
+      ["쇼핑몰?루", `https://res.CLOUDINARY_CLIENT_ID.com/${CLOUDINARY_CLIENT_ID}/image/upload/v1637874000/1637873933719_fxvncd.jpg`],
+      ["뿅!", `https://res.CLOUDINARY_CLIENT_ID.com/${CLOUDINARY_CLIENT_ID}/image/upload/v1637874683/images_1_qzx3hm.jpg`],
+      ["몰?루없음", `https://res.CLOUDINARY_CLIENT_ID.com/${CLOUDINARY_CLIENT_ID}/image/upload/v1637874661/images_bntlm7.jpg`],
+      ["모모?코", `https://res.CLOUDINARY_CLIENT_ID.com/${CLOUDINARY_CLIENT_ID}/image/upload/v1637874697/i14615767089_r4plwp.png`]
     ];
     const rand = (Math.random()*4).toFixed();
     
@@ -442,7 +476,7 @@ function onMessage(msg) {
       link_ver: '4.0',
       template_id: 65868,
       template_args: {
-        "IMG1": `https://res.cloudinary.com/${secret.cloudinary}/image/upload/v1637819645/1637601286501_revroy.jpg`,
+        "IMG1": `https://res.CLOUDINARY_CLIENT_ID.com/${CLOUDINARY_CLIENT_ID}/image/upload/v1637819645/1637601286501_revroy.jpg`,
         "TITLE": "몰?루",
         "CAT1": "몰루???",
         "ITEM": molu[Math.min(4,rand)][0],
@@ -450,6 +484,245 @@ function onMessage(msg) {
       }
     }, 'custom');
   }
+}
+
+const daum = function (search, page) {
+  const obj = {};
+  const list = JSON.parse(
+    new java.lang.String(Jsoup.connect("https://dapi.kakao.com/v2/search/web")
+      .data("query", search)
+      .header("Authorization", "KakaoAK "+NAVER_KEY) 
+      .ignoreContentType(true)
+      .ignoreHttpErrors(true) 
+      .execute().bodyAsBytes(), "UTF-8")).documents;
+  
+  obj["TITLE"] = "Daum web search: "+search+" ("+(page+1)+"/"+(list.length/5).toFixed()+")";
+  list.slice(5*page, 5*(page+1)).forEach((l,i)=>{
+    obj["LIST"+(i+1)] = (l.title||"").replace(/[<b>|</b>]/g,"");
+    obj["DESC"+(i+1)] = (l.contents||"").replace(/<b>|<\/b>/g,"");
+    obj["LINK"+(i+1)] = "redirect?url="+l.url;
+  });
+  return obj;
+}
+
+const melon = function (search, page) {
+  const obj = {};
+  const list = requestObj({
+    url: "https://api.music.msub.kr",
+    data: {
+      song: search
+    }
+  }).song;
+  
+  obj["TITLE"] = "Melon song search: "+search+" ("+(page+1)+"/"+(list.length/5).toFixed()+")";
+  list.slice(5*page, 5*(page+1)).forEach((l,i)=>{
+    obj["LIST"+(i+1)] = l.name;
+    obj["DESC"+(i+1)] = l.artist+" | "+l.album;
+    obj["IMG"+(i+1)] = l.albumimg;
+    obj["LINK"+(i+1)] = "redirect?url="+l.kakaomelonlink;
+  });
+  return obj;
+}
+
+const openchat = function (search, page) {
+  const obj = {};
+  const list = requestObj({
+    url: "https://api.develope.kr/search/room/list",
+    data: {
+      type: "m",
+      query: search
+    }
+  }).result.lists;
+ 
+  obj["TITLE"] = (search.length<=9?"Kakao room search: "+search:search)+" ("+(page+1)+"/"+(list.length/5).toFixed()+")";
+  list.slice(5*page, 5*(page+1)).sort((a,b)=>a.headcount<b.headcount?1:-1).forEach((l,i)=>{
+    obj["LIST"+(i+1)] = l.name;
+    obj["DESC"+(i+1)] = l.headcount+"명 | "+l.like+"❤";
+    obj["IMG"+(i+1)] = l.wp;
+    obj["LINK"+(i+1)] = "redirect?url="+l.openlink;
+  });
+  return obj;
+}
+
+const androidapp = function (search, page) {
+  const obj = {};
+  const list = requestObj({
+    url: "https://data.42matters.com/api/v2.0/android/apps/search.json",
+    data: {
+      access_token: MATTERS_KEY,
+      q: search
+    }
+  }).results;
+
+  obj["TITLE"] = (search.length<=8?"Android app search: "+search:search)+" ("+(page+1)+"/"+(list.length/5).toFixed()+")";
+  list.slice(5*page, 5*(page+1)).forEach((l,i)=>{
+    obj["LIST"+(i+1)] = l.title + " | "+ l.rating.toFixed(1)+"★";
+    obj["DESC"+(i+1)] = l.downloads +" 다운로드 | "
+      + (l.iap?"인앱결제 | ":"") 
+      + (l.contain_ads?"광고포함 | ":"");
+    obj["IMG"+(i+1)] = l.icon;
+    obj["LINK"+(i+1)] = "redirect?url="+l.market_url;
+  });
+  return obj;
+}
+
+const youtube = function (search, page, type) {
+  const obj = {};
+  const list = requestObj({
+    url: "https://www.googleapis.com/youtube/v3/search",
+    data: {
+      key: GOOGLE_KEY,
+      part: "snippet",
+      q: search,
+      type: type||"video"
+    }
+  }).items;
+  
+  obj["TITLE"] = "Youtube "+(type||"video")+" search: "+search+" ("+(page+1)+"/"+(list.length/5).toFixed()+")";
+  list.slice(5*page, 5*(page+1)).forEach((l,i)=>{
+    obj["LIST"+(i+1)] = l.snippet.title;
+    obj["DESC"+(i+1)] = l.snippet.publishTime.slice(0,10) + " | " + l.snippet.description;
+    obj["IMG"+(i+1)] = l.snippet.thumbnails.high.url;
+    obj["LINK"+(i+1)] = "redirect?url=https://youtube.com/video/"+l.id.videoId;
+  });
+  return obj;
+}
+
+const naverencyc = function (search) {
+  const list = requestObj({
+    url: "https://openapi.naver.com/v1/search/encyc.json",
+    data: {
+      query: search,
+      display: "10",
+      sort: "sim"
+    },
+    header: {
+      "X-Naver-Client-Id": NAVER_CLIENT_ID,
+      "X-Naver-Client-Secret": NAVER_CLIENT_SECRET
+    }
+  }).items;
+
+  return "Naver encyc search: "+search+"\n\n\n"+list.map((e,i)=>
+     e.title.replace(/<b>|<\/b>/g, "")
+       +"\n"+e.description.replace(/<b>|<\/b>|\\n/g, "")
+       +"\n자세히 보기: "+e.link
+       +(i==0&&list.length>2?"\n\n 더 많은 검색결과 보기"+"\u200d".repeat(501):"")).join("\n\n");
+}
+
+const google = function (search, page) {
+  const obj = {};
+  const list = requestObj({
+    url: "https://customsearch.googleapis.com/customsearch/v1",
+    data: {
+      key: GOOGLE_KEY,
+      cx: SEARCH_ID,
+      q: search,
+      gl: "KR",
+      hl: "KO",
+      lr: "lang_ko"
+    }
+  }).items;
+    
+  obj["TITLE"] = "Google web search: "+search+" ("+(page+1)+"/"+(list.length/5).toFixed()+")";
+  list.slice(5*page, 5*(page+1)).forEach((l,i)=>{
+    obj["LIST"+(i+1)] = l.title;
+    obj["DESC"+(i+1)] = l.snippet;
+    obj["IMG"+(i+1)] = l.pagemap.cse_image[0].src;
+    obj["LINK"+(i+1)] = "redirect?url="+l.link;
+  });
+  return obj;
+}
+
+const github = function (search, page, options) {
+  let gitype = (options.find(e=>e.startsWith("t:"))||"t:repo").slice(2);
+  gitype = gitype=="topic"?"topics":gitype=="user"?"users":"repositories";
+  let sort = (options.find(e=>e.startsWith("s:"))||"s:star").slice(2);
+
+  const httpreq = url => requestObj({
+    url: url,
+    header: {
+      "Authorization": java.lang.String("token "+GITHUB_KEY)
+    }
+  });
+
+  let v = httpreq("https://api.github.com/search/"+(gitype||"repositories")+"?q="+search);
+  let list = v.items;
+  
+  if(gitype=="users") list = httpreq(list.shift().repos_url);
+  if(sort=="star") list = list.sort((r1,r2)=>parseInt(r1.stargazers_count)<parseInt(r2.stargazers_count)?1:-1);
+  
+  return "Github "+(gitype||"repo")+" search: "+search+" ("+(page+1)+"/"+(Math.max(1,list.length/5)).toFixed()+")\n\n"+
+  list.slice(5*page, 5*(page+1)).map((r,i)=>{
+   let langs = httpreq(r.languages_url);
+   let total = Object.keys(langs).length&&Object.keys(langs).map(k=>langs[k]).reduce((a,e)=>a+e);
+   let tags = [];
+   if(r.archive) tags.push("읽기 전용");
+   if(r.disabled) tags.push("비활성화");
+   if(r.allow_forking) tags.push("복제 가능");
+   if(r.fork) tags.push("복제본");
+   if(r.private) tags.push("비공개");
+   if(r.is_template) tags.push("템플릿");
+   
+   return r.full_name
+    +"\n• 기본 정보: "
+      +r.forks+" 포크 | "
+      +r.open_issues_count+" 이슈 | "
+      +r.stargazers_count+" 스타 | "
+      +httpreq(r.subscribers_url).length+" 구독자 | "
+      +httpreq(r.contributors_url).length+" 기여자"
+    +(tags.length?"\n• 유형: "+tags.join(" | "):"")
+    +(r.topics.length?"\n• 토픽: "+r.topics.map(e=>"#"+e).join(" | "):"")
+    +(total?"\n• 언어 구성: "+Object.keys(langs).map((k)=>k+": "+(langs[k]/total*100).toFixed(2)+"%").join(" | "):"")
+    +(r.license?"\n• 라이선스: "+r.license.spdx_id:"")
+    +"\nby "+r.owner.login
+    +"\n"+(r.description||"")
+    +(i==0&&list.length>1?"\n\n 더 많은 검색결과 보기"+"\u200d".repeat(501):"");
+  }).join("\n\n\n");
+}
+
+const search = function (msg, args) {
+  let [ type, search, options ] = args;
+  let option = options&&options.replace(/\s/g,"").split(",");
+  let page = option?parseInt((option.find(e=>e.startsWith("p:"))||"p:1").slice(2))-1:0;
+  let obj = {};
+  
+  switch(type) {
+    case "유튜브": 
+      obj = youtube(search, page);
+      break;
+    case "멜론":
+      obj = melon(search, page);
+      break;
+    case "다음":
+      obj = daum(search, page);
+      break;
+    case "사전": 
+      obj = naverencyc(search);
+      break;
+    case "구글": 
+      obj = google(search, page);
+      break;
+    case "플스": 
+      obj = androidapp(search, page);
+      break;
+    case "카톡":
+      obj = openchat(search, page);
+      break;
+    case "깃허브": 
+      obj = github(search, page, option);
+      break;
+    default: msg.reply("Invalid type!");
+  }
+  
+  if(typeof obj === "object")
+    Kakao.send(msg.room, {
+      link_ver: "4.0",
+      template_id: 65892,
+      template_args: obj
+    }, 'custom');
+  else if(typeof obj === "string") 
+    msg.reply(obj);
+  else msg.reply("Empty object!");
 }
 
 init();
@@ -476,7 +749,7 @@ bot.addListener(Event.MESSAGE, msg=>{
   if(msg.content == "사진을 보냈습니다.") {
     const imagename = "test"+Date.now();
     const base64 = 'data:image/png;base64,'+msg.image.getBase64();
-    org.jsoup.Jsoup.connect(`https://api.cloudinary.com/v1_1/${secret.cloudinary}/image/upload`) 
+    org.jsoup.Jsoup.connect(`https://api.CLOUDINARY_CLIENT_ID.com/v1_1/${CLOUDINARY_CLIENT_ID}/image/upload`) 
       .data('file', base64) 
       .data('upload_preset','kakaoupload') 
       .data('public_id', imagename) 
